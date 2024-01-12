bfb117c7a823c7dde48f0647c080b5d24a7ba86e
Ваагн Киракосян <kirakosya.vage@gmail.com>
2024-01-12 12:29:06 +0300

    initial commit

---
+++ FoodDelivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Ваагн Киракосян on 12.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
